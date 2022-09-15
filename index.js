c70731fa36950f829522692877398a0733eefc95
eliel <elieldscalore@gmail.com>
2022-09-15 16:42:27 -0300

    backend ok

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require("cors");
 require('dotenv').config();
 
+app.use(express.static('build'));
 app.use(express.json());
 app.use(cors());
 
--- models/user.model.js
+++ models/user.model.js
@@ -4,7 +4,6 @@ const UserSchema = new Schema ({
     username: {type:String, required:true, trim: true },
     name: {type: String, required: true, trim:true, minLength: 3},
     passwordHash: {type: String, required:true, trim:true, minLength: 3},
-    blogs:[{ type:  Types.ObjectId, ref: "Blog"}]
 })
 
 const UserModel = model("User", UserSchema);
