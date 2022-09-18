17430f9e47b72e7dfd22048187fff34ac20dbb9f
eliel <elieldscalore@gmail.com>
2022-09-18 10:12:38 -0300

    refactor: update index.js

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const app = express();
 const cors = require("cors");
 require('dotenv').config();
 
-app.use(express.static('build'));
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: process.env.REACT_APP_URL }));
+
 
 const CepRouter = require("./controllers/cep.router");
 app.use("/cep", CepRouter);
